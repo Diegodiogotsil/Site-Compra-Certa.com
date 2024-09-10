28334a02af683e6e271ee52e063f575a1150c493
diegodiogotsil <diegotsil12@gmail.com>
2024-09-10 18:14:25 +0000

    realizado alterações para adição de api

--- Site_compras/src/components/pages/Cadastro.js
+++ Site_compras/src/components/pages/Cadastro.js
@@ -1,23 +1,41 @@
 import './Cadastro.css';
-import lixeira from '../../imagens/lixeira.jpg';
-function Cadastro (){
+import api from '../../servicos/Api';
+import lixeira from '../../img/lixeira2.png';
+import axios from 'axios';
+import { useEffect } from 'react';
+function Cadastro() {
+    let users = []
+
+    async function getUsers(){
+        users = await api.get ('/usuarios')
+    }
+
+    useEffect (() =>{
+        getUsers()
+    }, [])
     return (
-        <div>
-            <form>
+        <div className='container-cadastro'>
+            <form className='form-cadastro'>
                 <h1>Cadastro de Usuarios</h1>
-                <input name='nome' type='text'/>
-                <input name='idade' type='number'/>
-                <input name='e-mail' type='email'/>
+                <input placeholder='Nome' name='nome' type='text' />
+                <input placeholder='Idade' name='idade' type='number' />
+                <input placeholder='E-mail' name='e-mail' type='email' />
                 <button type='button'>Cadastrar</button>
             </form>
-            <div>
-                <div>
 
+            {users.map(user => (
+                <div key={user.id} className='card-cadastro'>
+                    <div>
+                        <p>Nome: <span>{user.name}</span></p>
+                        <p>Idade: <span>{user.age}</span></p>
+                        <p>E-Mail: <span>{user.email}</span></p>
+                    </div>                   
+                    <button>
+                        <img src={lixeira} />
+                    </button>
                 </div>
-                <button>
-                    <img src={lixeira}/>
-                </button>
-            </div>
+            ))}
+
         </div>
     )
 }
